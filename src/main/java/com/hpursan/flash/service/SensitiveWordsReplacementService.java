a6e03e6462df37d691c3d2268de88d89874633ad
hpursan <hpursan@gmail.com>
2024-05-04 19:16:11 +0200

    Added logging

--- src/main/java/com/hpursan/flash/service/SensitiveWordsReplacementService.java
+++ src/main/java/com/hpursan/flash/service/SensitiveWordsReplacementService.java
@@ -8,29 +8,33 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SensitiveWordsReplacementService {
     private final SensitiveWordsMaintenanceService sensitiveWordsMaintenanceService;
 
-    public String redactSensitiveWords(String message){
+    public String redactSensitiveWords(String message) {
+        log.info("Redacting sensitive words from message: {}", message);
 
-        // get a list of the sensitive words
+        // Get a list of sensitive words
         List<SensitiveWord> sensitiveWords = sensitiveWordsMaintenanceService.listAllSensitiveWordsOrderByLengthWord(false);
 
         if (sensitiveWords.isEmpty()) {
+            log.info("No sensitive words found. Skipping redaction.");
             return message;
         }
 
         for (SensitiveWord sensitiveWord : sensitiveWords) {
-
-            // Going to need the wrap the phrase with escape character
             String phrase = sensitiveWord.getWord();
 
             // Escape special characters in the phrase
             String escapedPhrase = Pattern.quote(phrase);
 
-            // Now, we can compile the pattern with the entire phrase
+            // Compile the pattern with the entire phrase
             Pattern pattern = Pattern.compile("\\b" + escapedPhrase + "\\b", Pattern.CASE_INSENSITIVE);
 
             // Replace occurrences of the sensitive phrase with redaction character
@@ -42,9 +46,12 @@ public class SensitiveWordsReplacementService {
             }
             matcher.appendTail(sb);
             message = sb.toString();
+
+            log.debug("Sensitive word '{}' redacted from message", phrase);
         }
 
+        log.info("Sensitive words redacted. Final message: {}", message);
         return message;
     }
-
 }
+
