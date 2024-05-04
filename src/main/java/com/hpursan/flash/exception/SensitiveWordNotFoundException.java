3c2d6a3c98eecefd46dd28b23f7b7eedb61902d6
hpursan <hpursan@gmail.com>
2024-05-04 11:01:15 +0200

    Removed unused exception method and clean up

--- src/main/java/com/hpursan/flash/exception/SensitiveWordNotFoundException.java
+++ src/main/java/com/hpursan/flash/exception/SensitiveWordNotFoundException.java
@@ -1,9 +1,9 @@
 package com.hpursan.flash.exception;
 
 public class SensitiveWordNotFoundException extends RuntimeException{
-    public SensitiveWordNotFoundException(String word) {
-        super("Sensitive word " + word + " not found");
-    }
+//    public SensitiveWordNotFoundException(String word) {
+//        super("Sensitive word " + word + " not found");
+//    }
 
     public SensitiveWordNotFoundException(Long id) {
         super("Sensitive word with id " + id + " not found");
